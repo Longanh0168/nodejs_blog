b86666f0bc669366d39a1866dcca4b9089132f9f
Thành Long <thanhlong11a11@gmail.com>
2023-01-31 15:22:42 +0700

    Install prettier

--- src/index.js
+++ src/index.js
@@ -6,22 +6,22 @@ const app = express();
 const port = 3000;
 
 // HTTP logger
-    app.use(morgan('combined'));
+app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', engine({ extname: '.hbs' }));
-app.set(               'view engine', 'hbs');
-               app.set('views', path.join(__dirname, 'resources/views'));
+app.set('view engine            ', 'hbs');
+app.set(            'views', path.join(__dirname, 'resources/views'));
 
 // method
-                                       app.get('/', (req, res) => {
-  res.render('home');
+app.get('/', (req, res) => {
+            res.render('home');
 });
 
-app.get('/news', (req, res) => {
-  res.render('news');
+        app.get('/news', (req,                  res) => {
+    res.render('news');
 });
 
-app.listen         (port, () =>
-          console.log(`Example app listening at http://localhost:${port}`),
+           app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`),
 );
